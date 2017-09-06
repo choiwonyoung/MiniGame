d8b325284db8ad72039336f24fa4192fbeef6908
ChoiWonYoung <uce2000@naver.com>
2017-09-07 07:24:21 +0900

    Image
    Work

--- Image.h
+++ Image.h
@@ -4,5 +4,15 @@ class Image
 public:
 	Image();
 	~Image();
+
+	int width();
+	int height();
+
+	void Draw( int dstX, int dstY, int srcX, int srcY , int width , int height ) const;
+
+private:
+	int m_iWidth;
+	int m_iHeight;
+	unsigned* m_pData;
 };
 
